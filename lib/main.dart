2ee6f23a9452dec42674faaf5ba461f71491964b
Nicolas Bock <nicolasbock@gmail.com>
2021-10-11 07:50:39 -0600

    Add platform and some gravity
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_launcher_icons/android.dart';
 import 'package:flutter_launcher_icons/constants.dart';
@@ -15,8 +16,7 @@ void main() {
   runApp(GameWidget(game: game));
 }
 
-class Player extends SpriteComponent {
-  // creates a component that renders the crate.png sprite, with size 16 x 16
+class Player extends SpriteComponent with Hitbox, Collidable {
   Player() : super(size: Vector2.all(32));
 
   @override
@@ -24,21 +24,41 @@ class Player extends SpriteComponent {
     super.onLoad();
     sprite = await Sprite.load('ball.png');
     anchor = Anchor.center;
+    final hitbox = HitboxCircle(definition: 32);
+    addHitbox(hitbox);
   }
 
   @override
   void onGameResize(Vector2 gameSize) {
-    // We don't need to set the position in the constructor, we can it directly here since it will
-    // be called once before the first time it is rendered.
     super.onGameResize(gameSize);
     position = gameSize / 2;
   }
 }
 
-class SunflowerDash extends FlameGame {
+class Platform extends SpriteComponent with Hitbox, Collidable {
+  Platform() : super(size: Vector2(128, 32));
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    sprite = await Sprite.load('platform.png');
+    anchor = Anchor.center;
+    final hitbox = HitboxRectangle(relation: Vector2(128, 32));
+    addHitbox(hitbox);
+  }
+
+  @override
+  void onGameResize(Vector2 gameSize) {
+    super.onGameResize(gameSize);
+    position = Vector2(gameSize[0] / 2, gameSize[1] - 100);
+  }
+}
+
+class SunflowerDash extends FlameGame with HasCollidables {
   @override
   Future<void> onLoad() async {
     super.onLoad();
     add(Player());
+    add(Platform());
   }
 }
